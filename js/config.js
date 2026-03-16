3ed5853823491a834c5f11cfcc3c4b2ac2301a91
Robson Jorge de Matos <robson.matos@seasc.sc.gov.br>
2026-03-16 14:03:06 -0300

    feat: replace tools with patrons in menu, update landpage and create patronos page with MP integration

--- js/config.js
+++ js/config.js
@@ -13,3 +13,11 @@ export const firebaseConfig = {
 export const imgbbConfig = {
     apiKey: "d0a234b1b52906d4a518c8a0e8646c3a"
 };
+
+// Mercado Pago Configuration
+export const mercadopagoConfig = {
+    publicKey: "APP_USR-4f9ff51f-75bd-45d1-8bcd-154f87179b4b",
+    accessToken: "APP_USR-4800153300194260-031608-db2dd707efafbdde834e61349a2024ab-93943184",
+    clientId: "4800153300194260",
+    clientSecret: "VdRcsU2zSwhVhE1Ns64D29qnZNiyyUmI"
+};
